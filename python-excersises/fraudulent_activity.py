b4afda80beb8853372dc8bd034649c5e201c1904
Vital V <uveuve@gmail.com>
2018-10-25 13:54:24 +0200

    finished excersise on fraudulent activity

--- python-excersises/fraudulent_activity.py
+++ python-excersises/fraudulent_activity.py
@@ -1,26 +1,31 @@
 import numpy as np 
 
-data = open('input0.txt').readlines()
-n, d = int(data[0].split()[0]), int(data[0].split()[1])
-t = [int(n) for n in data[1].split()]
+def read_data(filename):
+	data = open(filename).readlines()
+	n, d = int(data[0].split()[0]), int(data[0].split()[1])
+	t = [int(n) for n in data[1].split()]
+	return n, d, t
 
 
-#find d+1 position in list 
-
-dix = d-1 #find preceding number for day d
-t[dix::-1][0:d] #find the preciding d numbers from dix
-#and continue with following days
-t[dix+1::-1][0:d]
-
-#do this until last element in list
+n,d,t = read_data('input0.txt')
 
 
-for i in range(d-1, len(t)-1):
-	m = np.median(t[i::-1][0:d])
-	c = t[i+1]
-	if c >= 2*m:
-		print("Slow down with the credit card!")
+def count_notifications(filename):
+    n,d,t = read_data(filename)
+    return len([t[i+1] for i in range(d-1, len(t)-1) if t[i+1] >= 2*(np.median(t[i::-1][0:d]))])
 
-#fancy one-liner:
-[t[i+1] for i in range(d-1, len(t)-1) if t[i+1] >= 2*(np.median(t[i::-1][0:d]))]
 
+
+#Longer but also more readable solution:
+def count_notifications_2(filename):
+	n,d,t = read_data(filename)
+	notifications=0
+	for i in range(d-1, len(t)-1):
+		m = np.median(t[i::-1][0:d])
+		print("\nMedian preceding %d days: %d"%(d, m))
+		c = t[i+1]
+		print("Expenses current day: %d"%c)
+		if c >= 2*m:
+			print("Slow down with the credit card!")
+			notifications+=1
+	return notifications
